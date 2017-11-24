078b608047e2be8a511509d0d7fe85a071a788ce
ccm2012 <ccm.hsss.2012@gmail.com>
2017-11-24 11:03:12 +0700

    Reviewing

--- app/models/book.rb
+++ app/models/book.rb
@@ -8,10 +8,19 @@ class Book < ApplicationRecord
   has_many :bill_details, dependent: :destroy
   has_many :reading_histories, dependent: :destroy
 
+  mount_uploader :picture, PictureUploader
   validates :author, presence: true,
     length: {maximum: Settings.max_length.name}
   validates :content, presence: true
   validates :summary, presence: true
   validates :title, presence: true,
     length: {maximum: Settings.max_length.title}
+  validate :picture_size
+
+  private
+
+  def picture_size
+    return unless picture.size > 5.megabytes
+    errors.add(:picture, t("review_for.picture_size"))
+  end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,22 @@
 class Review < ApplicationRecord
+  ATTRIBUTE_PARAMS = %i(user_id book_id rate content picture)
+
+  scope :set_order, -> {order("created_at desc")}
+
   belongs_to :user
   belongs_to :book
 
   has_many :comments, dependent: :destroy
+
+  mount_uploader :picture, PictureUploader
+  validates :content, presence: true
+  validates :rate, presence: true
+  validate :picture_size
+
+  private
+
+  def picture_size
+    return unless picture.size > 5.megabytes
+    errors.add(:picture, t("review_for.picture_size"))
+  end
 end
